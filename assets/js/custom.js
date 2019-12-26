17c65729ec6dc04c4b780f0be9c29ce6975abe40
johackim <contact@johackim.com>
2019-12-26 19:41:36 +0100

    Add testimonials

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,3 +3,13 @@ var titles = document.querySelectorAll('.post-card-title');
 [].forEach.call(titles, function(title) {
     title.innerHTML = title.innerHTML.replace(" ?", "&nbsp;?");
 });
+
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+});
